581c29d6d4033c3ac438ddef8419708a3c584c83
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-18 18:09:01 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+/**
+ * Created by starsky on 4/18/17.
+ */
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+// let user = {name: 'starsky', age: 35};
+// let {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //    console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').deleteMany({name: 'Starsky Lara'});
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('58f5c140d8a45f0e93773db8')
+    }).then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    })
+
+    // db.close();
+});
