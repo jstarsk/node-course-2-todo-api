80a9f8c5fdf21e1fa747b3b98168453d0c4abc9b
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-19 18:14:54 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 /**
  * Created by starsky on 4/18/17.
  */
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -69,6 +70,37 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.complete) && body.complete) {
+        body.completeAt = new Date().getTime();
+    } else {
+        body.complete = false;
+        body.completeAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
