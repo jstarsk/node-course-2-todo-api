aaad99282cbba0941bbe8cd17f81389f09b1a732
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-19 12:10:36 +0200

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
  */
 let express = require('express');
 let bodyParser = require('body-parser');
+let {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -33,6 +34,23 @@ app.get('/todos', (req, res) => {
     });
 });
 
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
