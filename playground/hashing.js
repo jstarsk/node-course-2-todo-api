a427aa9a48b7bd15b79340bcc8056d49f2874c98
Jose Starsky Naya Lara <starsky@Joses-MacBook-Pro.local>
2017-04-24 15:37:52 +0200

    Hash user password before saved

--- playground/hashing.js
+++ playground/hashing.js
@@ -3,15 +3,29 @@
  */
 const {SHA256} = require('crypto-js');
 const  jwt = require('jsonwebtoken');
+const bycrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+let password = '123abc';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
-let decode = jwt.verify(token, '123abc');
-console.log(decode);
+// bycrypt.genSalt(10, (err, salt) => {
+//     bycrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
+
+let hashedPassword ='$2a$10$Gi8ngqbEoSdKz3zgrgAdb.5KDMfLa1BbO3gDmnaZIb0Fp5u.48cui';
+
+bycrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+// let data = {
+//     id: 10
+// };
+//
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+// let decode = jwt.verify(token, '123abc');
+// console.log(decode);
 //
 // let message = 'I am user number';
 // let hash = SHA256(message).toString();
